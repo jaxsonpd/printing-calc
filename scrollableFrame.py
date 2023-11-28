a7bc8e0085376ac335f2341c920a320435252a01
Jack Duignan <Jackpduignan@gmail.com>
2023-11-28 19:23:40 +1300

    scrollable frame able to resize

--- scrollableFrame.py
+++ scrollableFrame.py
@@ -11,33 +11,48 @@ from tkinter import ttk
 
 ## @class ScrollableFrame
 # @brief The scrollable frame class
-class ScrollableFrame(ttk.Frame):
+class ScrollableFrame():
     ## @brief The constructor for the ScrollableFrame class
     # @param self The object pointer
     # @param master The master widget to place the scrollable frame in
     def __init__(self, master: ttk.Widget= None) -> None:
-        super().__init__(master)
+        self.outer = tk.Frame(master)
+        self.outer.grid_columnconfigure(0, weight=1)
+        self.outer.grid_rowconfigure(0, weight=1)
 
         # Create the canvas
-        self.canvas = tk.Canvas(self)
+        self.canvas = tk.Canvas(self.outer, borderwidth=0, highlightthickness=0, takefocus=0)
 
         # Create the scroll bar
-        self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.scrollbar = ttk.Scrollbar(self.outer, orient="vertical", command=self.canvas.yview)
 
         # Configure the canvas
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.bind("<Configure>", self._resize_interior)
 
         # Create the frame
-        self.frame = ttk.Frame(self.canvas)
-        self.canvas.create_window(0, 0, anchor='nw', window=self.frame)
-        self.frame.bind("<Configure>", lambda event: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+        self.interior = tk.Frame(self.canvas, bg="red")
+        self.interior_id = self.canvas.create_window(0, 0, anchor='nw', window=self.interior)
+        self.interior.bind("<Configure>", self._update_scroll_region)
 
-        self.canvas.pack(fill='both', expand=True, side='left')
-        self.scrollbar.pack(fill='y', side='right')
-        self.frame.pack(fill='both', expand=True)
+        # Layout
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.scrollbar.grid(row=0, column=1, sticky="ns")
 
-    def return_frame(self):
-        return self.frame
+    def _resize_interior(self, event) -> None:
+        if self.interior.winfo_reqwidth() != self.interior.winfo_reqheight():
+            # Update the inner frames width to fill the canvas
+            self.canvas.itemconfigure(self.interior_id, width=self.canvas.winfo_width())
+            
+    
+
+    def _update_scroll_region(self, event) -> None:
+        # update scrollbar to match the size of the inner frame
+        size = (self.interior.winfo_reqwidth(), self.interior.winfo_reqheight())
+        self.canvas.config(scrollregion="0 0 %s %s" % size)
+        if self.interior.winfo_reqwidth() != self.canvas.winfo_width():
+            # update the canvas's width to fit the interior frame
+            self.canvas.config(width=self.interior.winfo_reqwidth())
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -46,11 +61,11 @@ if __name__ == "__main__":
 
     # Create the scrollable frame
     scrollable_frame = ScrollableFrame(root)
-    scrollable_frame.grid(row=0, column=0, sticky="nsew")
+    scrollable_frame.outer.grid(row=0, column=0, sticky="nsew")
 
     # Create the widgets
     for i in range(50):
-        ttk.Label(scrollable_frame.frame, text=f"Label {i}").grid(row=i, column=0)
+        ttk.Label(scrollable_frame.interior, text=f"Label {i}").grid(row=i, column=0)
 
     root.mainloop()
 
